994c4dce401759ae0d3a0bddd7e005e7d131af10
Todd Stout <todd.tstout@gmail.com>
2013-05-29 07:41:02 -0500

    added license

--- ap-test/src/main/java/aptools/test/Javac.java
+++ ap-test/src/main/java/aptools/test/Javac.java
@@ -1,3 +1,17 @@
+//  Copyright 2013 Todd Stout
+//
+//  Licensed under the Apache License,Version2.0(the"License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing,software
+//  distributed under the License is distributed on an"AS IS"BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
 package aptools.test;
 
 import com.google.common.io.Files;
@@ -15,7 +29,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
-import static com.google.common.base.Throwables.propagate;
+import static com.google.common.base.Throwables.*;
 import static com.google.common.collect.Lists.*;
 
 /**
